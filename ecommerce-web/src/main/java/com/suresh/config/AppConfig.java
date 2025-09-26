efd9de3d02d0746070f991f5269db083657971ca
suresh <suresh123@gmail.com>
2025-09-26 17:27:43 +0530

    security enables

--- ecommerce-web/src/main/java/com/suresh/config/AppConfig.java
+++ ecommerce-web/src/main/java/com/suresh/config/AppConfig.java
@@ -1,4 +1,65 @@
 package com.suresh.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        http.sessionManagement(mangement -> mangement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )).authorizeHttpRequests(authorze -> authorze.requestMatchers(
+                                "/api/**"
+                        ).authenticated().requestMatchers("/api/product/*/review").permitAll()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        return http.build();
+    }
+
+    @Bean
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration   cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Collections.singletonList("*"));
+                cfg.setMaxAge(36000l);
+                return cfg;
+            }
+        };
+        //return null;
+
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
