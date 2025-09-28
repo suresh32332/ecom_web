569073a6430bc385e9da3ae7ba1e0d9ad4b1b929
suresh <suresh123@gmail.com>
2025-09-28 16:26:32 +0530

    auth

--- ecommerce-web/src/main/java/com/suresh/config/JwtProvider.java
+++ ecommerce-web/src/main/java/com/suresh/config/JwtProvider.java
@@ -1,10 +1,22 @@
 package com.suresh.config;
 
 import io.jsonwebtoken.security.Keys;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.crypto.SecretKey;
+import java.util.Collection;
 
 public class JwtProvider {
 
     SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECKET_KEY.getBytes());
+        public String generateToken(Authorization auth){
+            Collection<? extends GrantedAuthority> authorities =auth.getAuthorities();
+            String role =popularAuthorities(authorities);
+
+            return null;
+        }
+
+    private String popularAuthorities(Collection<? extends GrantedAuthority> authorities) {
+    }
 }
